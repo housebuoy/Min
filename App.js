6c748ce0eeb07e7a50ca2e2125f821e64bb1cf06
Kelvin Quarcoo <kelvinquarcoo2005@gmail.com>
2024-06-10 00:32:09 +0000

    added some features such as the onboarding screen and sign up page

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import logo from './assets/images/logo.png';
 import WelcomeScreen from './screens/WelcomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Onboarding from './screens/Onboarding';
+import SignIn from './screens/SignIn';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <WelcomeScreen />
-    </View>
+     <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen 
+          name="Welcome" 
+          component={WelcomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        /> */}
+        <Stack.Screen 
+          name="Onboarding" 
+          component={Onboarding}
+          options={{
+            headerShown: false,
+          }} />
+        <Stack.Screen 
+          name="SignIn" 
+          component={SignIn}
+          options={{
+            headerShown: false,
+          }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
